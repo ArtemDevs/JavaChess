0866ccf70eee7b27e823cf6197cb7a0dee2d4c4d
Artem Ouliankine <artem.ouliankine@bellevuecollege.edu>
2019-12-27 16:53:54 -0800

    the final Piece, king, finished

---
+++ src/com/chess/engine/pieces/King.java
@@ -0,0 +1,65 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Loyalty;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Square;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece {
+
+  private static final int[] CANDIDATE_MOVE_CORD = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+  King(int piecePosition, Loyalty pieceLoyalty) {
+    super(piecePosition, pieceLoyalty);
+  }
+
+  @Override
+  public Collection<Move> calculateLegalMoves(Board board) {
+    final List<Move> legalMoves = new ArrayList<>();
+
+    for (final int currentCandidateOffset : CANDIDATE_MOVE_CORD) {
+      final int candidateDestinationCord = this.piecePosition + currentCandidateOffset;
+
+      // edge cases where we want to skip
+      if (isFirstColumnEdgeCase(this.piecePosition, currentCandidateOffset)
+          || isEighthColumnEdgeCase(this.piecePosition, currentCandidateOffset)) {
+        continue;
+      }
+
+      // get the square
+      final Square candidateDestinationSquare = board.getSquare(candidateDestinationCord);
+
+      // if the square is not occupied then we add a legal move to the list, and we can loop again
+      if (!candidateDestinationSquare.isSquareOccupied()) {
+        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCord));
+      } else {
+        final Piece pieceAtDestinationCord = candidateDestinationSquare.getPiece();
+        final Loyalty pieceLoyalty = pieceAtDestinationCord.getPieceLoyalty();
+
+        // if the loyalty is the opposite, we can add an attack move to the legal moves
+        if (this.pieceLoyalty != pieceLoyalty) {
+          legalMoves.add(
+              new Move.AttackMove(board, this, candidateDestinationCord, pieceAtDestinationCord));
+        }
+      }
+    }
+    return ImmutableList.copyOf(legalMoves);
+  }
+
+  // Edge cases for the Knight, for when near the board edge and the offset will be incorrect
+  private static boolean isFirstColumnEdgeCase(final int currentPos, final int candidateOffset) {
+    return BoardUtils.FIRST_COLUMN[currentPos]
+        && (candidateOffset == -9 || candidateOffset == -1 || candidateOffset == 7);
+  }
+
+  private static boolean isEighthColumnEdgeCase(final int currentPos, final int candidateOffset) {
+    return BoardUtils.EIGHTH_COLUMN[currentPos]
+        && (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9);
+  }
+}
--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -38,7 +38,7 @@ public class Pawn extends Piece {
           && !board.getSquare(candidateDestinationCord).isSquareOccupied()) {
         // TODO need to implement pawn promotions
         legalMoves.add(new MajorMove(board, this, candidateDestinationCord));
-      // if the pawn is jumping forwards from its starting position
+        // if the pawn is jumping forwards from its starting position
       } else if (candidateCordOffset == 16
               && this.isFirstMove()
               && BoardUtils.SECOND_ROW[this.piecePosition]
@@ -51,28 +51,30 @@ public class Pawn extends Piece {
         if (!board.getSquare(behindCandidateDestinationCord).isSquareOccupied()
             && !board.getSquare(behindCandidateDestinationCord).isSquareOccupied()) {
           legalMoves.add(new MajorMove(board, this, candidateDestinationCord));
-        // TODO might need to fix this later if edge case falls apart
-        } else if (candidateCordOffset == 7){
+          // TODO might need to fix this later if edge case falls apart
+        } else if (candidateCordOffset == 7) {
           if (isFirstColumnEdgeCase(this.piecePosition, candidateCordOffset, this.pieceLoyalty)
-                  || isEighthColumnEdgeCase(this.piecePosition, candidateCordOffset, this.pieceLoyalty)) {
+              || isEighthColumnEdgeCase(
+                  this.piecePosition, candidateCordOffset, this.pieceLoyalty)) {
             continue;
           }
-          if(board.getSquare(candidateDestinationCord).isSquareOccupied()){
+          if (board.getSquare(candidateDestinationCord).isSquareOccupied()) {
             final Piece pieceOnCandidate = board.getSquare(candidateDestinationCord).getPiece();
-            if(this.pieceLoyalty != pieceOnCandidate.getPieceLoyalty()){
+            if (this.pieceLoyalty != pieceOnCandidate.getPieceLoyalty()) {
               // TODO need to add attacking into a pawn promotion
               legalMoves.add(new MajorMove(board, this, candidateDestinationCord));
             }
           }
-        // TODO might need to fix this later if edge case falls apart
-        } else if (candidateCordOffset == 9){
+          // TODO might need to fix this later if edge case falls apart
+        } else if (candidateCordOffset == 9) {
           if (isFirstColumnEdgeCase(this.piecePosition, candidateCordOffset, this.pieceLoyalty)
-                  || isEighthColumnEdgeCase(this.piecePosition, candidateCordOffset, this.pieceLoyalty)) {
+              || isEighthColumnEdgeCase(
+                  this.piecePosition, candidateCordOffset, this.pieceLoyalty)) {
             continue;
           }
-          if(board.getSquare(candidateDestinationCord).isSquareOccupied()){
+          if (board.getSquare(candidateDestinationCord).isSquareOccupied()) {
             final Piece pieceOnCandidate = board.getSquare(candidateDestinationCord).getPiece();
-            if(this.pieceLoyalty != pieceOnCandidate.getPieceLoyalty()){
+            if (this.pieceLoyalty != pieceOnCandidate.getPieceLoyalty()) {
               // TODO need to add attacking into a pawn promotion
               legalMoves.add(new MajorMove(board, this, candidateDestinationCord));
             }
@@ -84,14 +86,15 @@ public class Pawn extends Piece {
     return ImmutableList.copyOf(legalMoves);
   }
 
-  private static boolean isFirstColumnEdgeCase(final int currentPos, final int candidateOffset, final Loyalty loyalty) {
-    return (candidateOffset == 7 && (BoardUtils.FIRST_COLUMN[currentPos] && loyalty.isBlack())) ||
-            (candidateOffset == 9 && (BoardUtils.FIRST_COLUMN[currentPos] && loyalty.isWhite()));
+  private static boolean isFirstColumnEdgeCase(
+      final int currentPos, final int candidateOffset, final Loyalty loyalty) {
+    return (candidateOffset == 7 && (BoardUtils.FIRST_COLUMN[currentPos] && loyalty.isBlack()))
+        || (candidateOffset == 9 && (BoardUtils.FIRST_COLUMN[currentPos] && loyalty.isWhite()));
   }
 
-  private static boolean isEighthColumnEdgeCase(final int currentPos, final int candidateOffset, final Loyalty loyalty){
-    return (candidateOffset == 7 && (BoardUtils.EIGHTH_COLUMN[currentPos] && loyalty.isWhite())) ||
-            (candidateOffset == 9 && (BoardUtils.EIGHTH_COLUMN[currentPos] && loyalty.isBlack()));
-
+  private static boolean isEighthColumnEdgeCase(
+      final int currentPos, final int candidateOffset, final Loyalty loyalty) {
+    return (candidateOffset == 7 && (BoardUtils.EIGHTH_COLUMN[currentPos] && loyalty.isWhite()))
+        || (candidateOffset == 9 && (BoardUtils.EIGHTH_COLUMN[currentPos] && loyalty.isBlack()));
   }
 }
